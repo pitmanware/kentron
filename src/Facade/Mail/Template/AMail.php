bd1d72577cd350da9946f899954770bcddd2840f
Ben Pitman <pitman.ben@gmail.com>
2020-11-29 16:04:13 +0000

    Bug fix to abstract method declaration

--- src/Facade/Mail/Template/AMail.php
+++ src/Facade/Mail/Template/AMail.php
@@ -10,9 +10,9 @@ use Kentron\Facade\Mail\Entity\MailTransportEntity;
 abstract class AMail
 {
     abstract public static function send (MailTransportEntity $mailTransportEntity): bool;
-    abstract public static function addRecipient (MailTargetEntity $mailTargetEntity): bool;
-    abstract public static function attach (MailAttachmentEntity $mailAttachmentEntity): bool;
-    abstract public static function embed (MailEmbedEntity $mailEmbedEntity): bool;
+    abstract protected static function addRecipient (MailTargetEntity $mailTargetEntity): bool;
+    abstract protected static function attach (MailAttachmentEntity $mailAttachmentEntity): bool;
+    abstract protected static function embed (MailEmbedEntity $mailEmbedEntity): bool;
 
     public static function sendMail (MailTransportEntity $mailTransportEntity): bool
     {
